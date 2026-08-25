19cc03a26db9b1dc7d981316c4a066866ea5bc51
MASK 😷 Official <mantisbro7012@gmail.com>
2026-08-25 18:11:08 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,46 @@
+class script(object):
+    START_TXT = """<b><i>🥀Hello {}, Nice to meet you👀</i></b>
+    
+<b><i>I can provide movies and Web-Series for you just make to admin your group and Ues me..💫</i></b>
+<b>〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰</b>
+<b><a href=https://t.me/+iEbhY7mM4oE1OTVl>©️𝐓𝐞𝐚𝐦 𝐂𝐢𝐧𝐞𝐦𝐚 𝐅𝐢𝐥𝐱™️</b></a>"""
+    
+    ABOUT_TXT = """<b>━━━❰ {} ❱━━━➣</b>
+    
+<b>○ <i>Owner Name : <a href=https://t.me/TG_x_filter>🇮🇳 𝗛𝗘𝗗𝗗𝗬 ◢ ◤</b></a></i>
+
+<b>○ <i>Creator : <a href=tg://settings>My Friend</b></a></i>
+
+<b>○ <i>Library : Pyrogram</b></i>
+
+<b>○ <i>Language : Python3</b></i>
+
+<b>○ <i>Data Base : MongoDB</b></i>
+
+<b>○ <i>Build Status : v5.2 [Stable]</b></i>
+
+<b>〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰</b>"""
+    
+    STATUS_TXT = """<b><u>📊 STATUS BOARD 📊</u></b>
+
+<b>➥ Total Files: <code>{}</code></b>
+<b>➥ Total Members: <code>{}</code></b>
+<b>➥ Total Chats: <code>{}</code></b>
+<b>➥ Used Storage: <code>{}</code></b>
+<b>〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰</b>"""
+    
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}
+"""
+
+    FILE_CAPTION = """<b><blockquote>📚 <code>{file_name}</code> </blockquote></b>
+    
+    <b>[©️𝐓𝐞𝐚𝐦 𝐂𝐢𝐧𝐞𝐦𝐚 𝐅𝐢𝐥𝐱™️](https://t.me/+iEbhY7mM4oE1OTVl)</b>"""
+
+    START_TXT_GROUP = """ഹലോ {}\n\n• എന്താണ് സഹോദരന് വേണ്ടത്? സ്റ്റാർട്ട്‌ ആയപ്പോ ഒരു സുഖം കിട്ടി അല്ലേ.. 🤭😜\n\n• എന്തായാലും വന്നതല്ലേ. ഇവിടെ കാണുന്ന ചാനൽ & ഗ്രൂപ്പ് ൽ ഒക്കെ Join ചെയ്തേക്ക് 😎"""
