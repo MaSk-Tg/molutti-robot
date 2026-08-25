99f2bf2ea0196fae8a967a30eef7a923d538da81
MASK 😷 Official <mantisbro7012@gmail.com>
2026-08-26 01:47:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-FSUB_CHANNELS = [int(ch)for ch in environ.get('FSUB_CHANNELS', '').split()if ch.strip()]
+FSUB_CHANNELS = [int(ch) for ch in environ.get('FSUB_CHANNELS', '').split()if ch.strip()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
